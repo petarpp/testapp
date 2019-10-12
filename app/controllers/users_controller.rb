f8b915cd74e9ed6d4a18903d8c9f9767bed8f613
petarpp <pf_petrov@yahoo.com>
2019-10-12 22:34:38 +0100

    created new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,7 +44,7 @@ class UsersController < ApplicationController
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
-      else1
+      else
         format.html { render :edit }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password)
+      params.require(:user).permit(:name, :email)
     end
 end
